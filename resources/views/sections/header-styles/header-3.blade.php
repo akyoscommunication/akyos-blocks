cfd0abc9205352d9f1fad013ee8704368a35e289
Kévin Groux <kevingroux@mac-mini-de-kevin.home>
2024-11-05 15:09:00 +0100

    merge

--- resources/views/sections/header-styles/header-3.blade.php
+++ resources/views/sections/header-styles/header-3.blade.php
@@ -1,39 +1,26 @@
 <header class="header-wrap">
-	<div class="container header">
+    <div class="container header">
 
-		<!-- LOGO -->
-		<div class="header-brand">
-			@if ($options['logo'])
-				<a href="{!! home_url() !!}">
-					<x-image :lg="$options['logo']" :sm="$options['logo_mobile']"></x-image>
-				</a>
-			@endif
-		</div>
-		<!----  --->
+        <!-- LOGO -->
+        <div class="header-brand">
+            @if ($options['logo'])
+				  <a href="{!! home_url() !!}">
+					  <x-image :lg="$options['logo']" :sm="$options['logo_mobile']"></x-image>
+				  </a>
+			  @endif
+		  </div>
+		 <!----  --->
 
-		<div class="header-nav">
-			<div id="burger">
-				<span></span>
-			</div>
-			<nav class="header-nav__burger">
-				@menu('main_navigation')
-			</nav>
-		</div>
+		 <div class="header-nav">
+			 @menu('main_navigation')
+		 </div>
 
-		@if(!empty($options['header_button']['url']))
-
-			<div class="header-actions">
-
-				@if(!empty($options['header_button']['url']))
-					<div class="header-actions-buttons">
-						<a href="{{ $options['header_button']['url'] }}">{{ $options['header_button']['title'] }}</a>
-					</div>
-				@endif
-
-				@if($options['social'])
-					@include('partials.social')
-				@endif
-			</div>
-		@endif
-  </div>
+		 @if(!empty($options['header_button']['url']))
+			 <div class="header-actions">
+				 <div class="header-actions-buttons">
+					 <a href="{{ $options['header_button']['url'] }}">{{ $options['header_button']['title'] }}</a>
+				 </div>
+			 </div>
+		 @endif
+    </div>
 </header>
