5aca5517730a94cc57d8be53510548606b0aa41f
Maxime Golfier <maxime.akyos@gmail.com>
2024-11-06 15:31:23 +0100

    fix content15

--- resources/views/blocks/contents/styles/content-15.blade.php
+++ resources/views/blocks/contents/styles/content-15.blade.php
@@ -1,10 +1,14 @@
 <div class="container s-content">
+    @if($content15['title']['value'])
     <x-title :tag="$content15['title']['tag']">
         {!! $content15['title']['value'] !!}
     </x-title>
+    @endif
+    @if($content15['content'])
     <div class="s-content__text">
         {!! $content15['content'] !!}
     </div>
+        @endif
 </div>
 
 
