35a935296b6f8f125169f9f147c208086a6b61b7
Kévin Groux <kevingroux@mac-mini-de-kevin.home>
2024-11-06 14:44:47 +0100

    change: content 6

--- resources/views/blocks/contents/styles/content-6.blade.php
+++ resources/views/blocks/contents/styles/content-6.blade.php
@@ -1,36 +1,32 @@
 <div class="container s-content-container">
+    <div class="container">
+        <div class="s-content__title">
+            <x-title :tag="$content6to7['title']['tag']">
+					{!! $content6to7['title']['value'] !!}
+				</x-title>
+		  </div>
 
-  <div class="container">
+		 <div class="s-content-layout">
+			 @foreach($content6to7['contents'] as $key => $content)
+				 <div class="s-content-layout-column">
+					 <x-title :tag="$content['title']['tag']">
+						 {!! $content['title']['value'] !!}
+					 </x-title>
 
-    <div class="s-content__title">
-      <x-title :tag="$content6to7['title']['tag']">
-        {!! $content6to7['title']['value'] !!}
-      </x-title>
-    </div>
+					 <div class="s-content__content">
+						 {!! $content['content'] !!}
+					 </div>
 
-    <div class="s-content-layout">
-      @foreach($content6to7['contents'] as $key => $content)
-        <div class="s-content-layout-column">
-          <x-title :tag="$content['title']['tag']">
-            {!! $content['title']['value'] !!}
-          </x-title>
-
-          <div class="s-content__content">
-            {!! $content['content'] !!}
-          </div>
-
-          @if($content['button']['link'])
-            <div class="s-content__button">
-              <x-button :appearance="$content['button']['color']" :icon="$content['button']['icon']" :iconposition="$content['button']['iconposition']"
-                        :href="$content['button']['link']['url']">
-                {{ $content['button']['link']['title'] }}
-              </x-button>
-            </div>
-          @endif
+					 @if($content['button']['link'])
+						 <div class="s-content__button">
+							 <x-akyos-blocks::aky-button :appearance="$content['button']['color']" :icon="$content['button']['icon']" :iconposition="$content['button']['iconposition']"
+										  :href="$content['button']['link']['url']">
+								 {{ $content['button']['link']['title'] }}
+							 </x-akyos-blocks::aky-button>
+						 </div>
+					 @endif
+				 </div>
+			 @endforeach
         </div>
-
-      @endforeach
     </div>
-
-  </div>
 </div>
