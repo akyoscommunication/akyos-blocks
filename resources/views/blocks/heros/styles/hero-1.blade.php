72a1efe0822b82809a9ed352baf8184945fb7c78
Kévin Groux <kevingroux@mac-mini-de-kevin.home>
2024-11-07 16:36:38 +0100

    start: hero slider

--- resources/views/blocks/heros/styles/hero-1.blade.php
+++ resources/views/blocks/heros/styles/hero-1.blade.php
@@ -1,14 +1,20 @@
-@php
-    use Akyos\Blocks\View\Components\AkyButton;
-
-      $akyButton = new AkyButton();
-@endphp
-
-@foreach($images as $key => $image)
-    <div class="s-hero-image">
-        <x-image :lg="$image['image']"/>
-    </div>
-@endforeach
+@if(count($images) > 1)
+    <x-slider name="slider--hero1" navigation="arrow" pagination="1" scrollbar="1" per="1" permd="1" persm="1"
+              perxs="1"
+              slider-id="slider--hero1">
+        @foreach($images as $image)
+            <div class="swiper-slide s-hero-image">
+                <x-image :lg="$image"/>
+            </div>
+        @endforeach
+    </x-slider>
+@else
+    @foreach($images as $image)
+        <div class="s-hero-image">
+            <x-image :lg="$image['image'] ?? $image"/>
+        </div>
+    @endforeach
+@endif
 <div class="container s-hero-layout">
     <div class="s-hero-title">
         <x-title :tag="$title['tag']">
@@ -23,17 +29,16 @@
     @if($buttons)
         <div class="s-hero-buttons">
             @foreach($buttons as $key => $button)
+                @component('akyos-blocks::components.aky-button', [
+            'appearance' => $button['button']['color'],
+            'icon' => $button['button']['icon'],
+            'iconposition' => $button['button']['iconposition'],
+            'href' => $button['button']['link']['url'],
+            'borderradius' => $button['button']['borderradius'],
+            'slot' => $button['button']['link']['title'],
+            ])
 
-                @php
-                    echo $akyButton->render()->with([
-              'appearance' => $button['button']['color'],
-              'icon' => $button['button']['icon'],
-              'iconposition' => $button['button']['iconposition'],
-              'href' => $button['button']['link']['url'],
-              'borderradius' => $button['button']['borderradius'],
-              'slot' => $button['button']['link']['title'],
-              ]);
-                @endphp
+                @endcomponent
             @endforeach
         </div>
     @endif
