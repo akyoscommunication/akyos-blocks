8000d9ed784c989db85b97f654707f93e0cab2a1
Kévin Groux <kevingroux@mac-mini-de-kevin.home>
2024-11-07 09:25:37 +0100

    fix: name fields

--- src/View/Blocks/AkyB_Numbers.php
+++ src/View/Blocks/AkyB_Numbers.php
@@ -2,8 +2,8 @@
 
 namespace Akyos\Blocks\View\Blocks;
 
-use Akyos\Blocks\Acf\Fields\AkyBNumbers1;
-use Akyos\Blocks\Acf\Fields\AkyBNumbers2;
+use Akyos\Blocks\Acf\Fields\AkyB_Numbers1;
+use Akyos\Blocks\Acf\Fields\AkyB_Numbers2;
 use Akyos\Core\Classes\Block;
 use Akyos\Core\Classes\GutenbergBlock;
 use Extended\ACF\ConditionalLogic;
@@ -12,46 +12,46 @@ use Extended\ACF\Fields\Tab;
 
 class AkyB_Numbers extends Block
 {
-	public function __construct()
-	{
-	}
+    public function __construct()
+    {
+    }
 
-	protected static function block(): GutenbergBlock
-	{
-		return (new GutenbergBlock())
-			->setName("akyb-numbers")
-			->setTitle("AKYB Liste de nombre")
-			->setCategory("content")
-			->setIcon("editor-ol");
-	}
+    protected static function block(): GutenbergBlock
+    {
+        return (new GutenbergBlock())
+            ->setName("akyb-numbers")
+            ->setTitle("AKYB Liste de nombre")
+            ->setCategory("content")
+            ->setIcon("editor-ol");
+    }
 
-	protected static function fields(): array
-	{
-		return [
-			Tab::make("Contenu"),
-			RadioButton::make('Style du bloc', 'style')
-				->choices([
-					'1' => 'Style 1 : <img style="max-width:300px;" src="'.get_template_directory_uri().'/resources/assets/images/numbers-style-1.png" alt="Style 1" />',
-					'2-1' => 'Style 2-1 : <img style="max-width:300px;" src="'.get_template_directory_uri().'/resources/assets/images/numbers-style-2-1.png" alt="Style 2-1" />',
-					'2-2' => 'Style 2-2 : <img style="max-width:300px;" src="'.get_template_directory_uri().'/resources/assets/images/numbers-style-2-2.png" alt="Style 2-2" />',
-				])->default('1'),
-			AkyBNumbers1::make('Nombre', 'number1')
-				->layout('block')
-				->conditionalLogic([
-					ConditionalLogic::where('style', '==', '1'),
-				]),
-			AkyBNumbers2::make('Nombre', 'number2')
-				->layout('block')
-				->conditionalLogic([
-					ConditionalLogic::where('style', '==', '2-1'),
-					ConditionalLogic::where('style', '==', '2-2'),
-				]),
+    protected static function fields(): array
+    {
+        return [
+            Tab::make("Contenu"),
+            RadioButton::make('Style du bloc', 'style')
+                ->choices([
+                    '1' => 'Style 1 : <img style="max-width:300px;" src="'.get_template_directory_uri().'/resources/assets/images/numbers-style-1.png" alt="Style 1" />',
+                    '2-1' => 'Style 2-1 : <img style="max-width:300px;" src="'.get_template_directory_uri().'/resources/assets/images/numbers-style-2-1.png" alt="Style 2-1" />',
+                    '2-2' => 'Style 2-2 : <img style="max-width:300px;" src="'.get_template_directory_uri().'/resources/assets/images/numbers-style-2-2.png" alt="Style 2-2" />',
+                ])->default('1'),
+            AkyB_Numbers1::make('Nombre', 'number1')
+                ->layout('block')
+                ->conditionalLogic([
+                    ConditionalLogic::where('style', '==', '1'),
+                ]),
+            AkyB_Numbers2::make('Nombre', 'number2')
+                ->layout('block')
+                ->conditionalLogic([
+                    ConditionalLogic::where('style', '==', '2-1'),
+                    ConditionalLogic::where('style', '==', '2-2'),
+                ]),
 
-		];
-	}
+        ];
+    }
 
-	public function render()
-	{
-		return $this->view("blocks.numbers.number");
-	}
+    public function render()
+    {
+        return $this->view("akyos-blocks::blocks.numbers.number");
+    }
 }
