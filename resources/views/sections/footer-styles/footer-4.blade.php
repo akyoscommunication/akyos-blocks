7711b96f5a39e2bc08435ef25e8121c026b0ca9d
Kévin Groux <kevingroux@mac-mini-de-kevin.home>
2024-11-14 16:48:02 +0100

    change css

--- resources/views/sections/footer-styles/footer-4.blade.php
+++ resources/views/sections/footer-styles/footer-4.blade.php
@@ -1,51 +1,50 @@
 <div class="container footer">
-  <div class="footer-layout">
-    <div class="footer-layout-inner">
-      <div class="footer-layout-inner__image">
-        <a class="" href="{!! home_url() !!}">
-			  <x-image :lg="empty($options['footer_logo']) ? $options['logo'] : $options['footer_logo']"></x-image>
-		  </a>
-		</div>
-		 <p>Follow Us</p>
-		 <div>
-			 @if($options['facebook'])
-				 <a href="{{ $options['facebook']}}" target="_blank">@icon("facebook")</a>
-			 @endif
-			 @if($options['instagram'])
-				 <a href="{{ $options['instagram']}}" target="_blank">@icon("instagram")</a>
-			 @endif
-			 @if($options['linkedin'])
-				 <a href="{{ $options['linkedin']}}" target="_blank">@icon("linkedin")</a>
-			 @endif
-			 @if($options['twitter'])
-				 <a href="{{ $options['twitter']}}" target="_blank">@icon("twitter")</a>
-			 @endif
-			 @if($options['youtube'])
-				 <a href="{{ $options['youtube']}}" target="_blank">@icon("youtube")</a>
-			 @endif
-		 </div>
-	 </div>
-	  <div class="footer-right">
-		  <span class="footer-bar"></span>
-
-		  <div class="footer-layout-info">
-			  <div class="">
-				  <p>{!! $options['address'] !!}</p>
-			  </div>
-			  <div class="footer-layout-info__text">
-				  {!! $options['footer_text'] !!}
-			  </div>
-		  </div>
-	  </div>
-  </div>
+    <div class="footer-layout">
+        <div class="footer-layout-inner">
+            <div class="footer-layout-inner__image">
+                <a class="" href="{!! home_url() !!}">
+                    <x-image :lg="empty($options['footer_logo']) ? $options['logo'] : $options['footer_logo']"></x-image>
+                </a>
+            </div>
+            <p>Follow Us</p>
+            <div>
+                @if($options['facebook'])
+                    <a href="{{ $options['facebook']}}" target="_blank">@icon("facebook")</a>
+                @endif
+                @if($options['instagram'])
+                    <a href="{{ $options['instagram']}}" target="_blank">@icon("instagram")</a>
+                @endif
+                @if($options['linkedin'])
+                    <a href="{{ $options['linkedin']}}" target="_blank">@icon("linkedin")</a>
+                @endif
+                @if($options['twitter'])
+                    <a href="{{ $options['twitter']}}" target="_blank">@icon("twitter")</a>
+                @endif
+                @if($options['youtube'])
+                    <a href="{{ $options['youtube']}}" target="_blank">@icon("youtube")</a>
+                @endif
+            </div>
+        </div>
+        <div class="footer-right">
+            <span class="footer-bar"></span>
+            <div class="footer-layout-info">
+                <div class="">
+                    <p>{!! $options['address'] !!}</p>
+                </div>
+                <div class="footer-layout-info__text">
+                    {!! $options['footer_text'] !!}
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <div class="footer-link">
-	<div class="">
-		@menu('legal_navigation')
-	</div>
-	<div class="">
+    <div class="">
+        @menu('legal_navigation')
+    </div>
+    <div class="">
           <span>© {{ bloginfo('name') }} {{ date('Y') }} | Une création <a
-              href="https://akyos.com" target="_blank">Akyos</a></span>
-  </div>
+                  href="https://akyos.com" target="_blank">Akyos</a></span>
+    </div>
 </div>
