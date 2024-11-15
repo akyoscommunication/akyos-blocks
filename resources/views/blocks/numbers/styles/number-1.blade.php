acf7fd87cabfdb04176d9ad610ada8f26f27bc30
Kévin Groux <kevingroux@mac-mini-de-kevin.home>
2024-11-15 16:24:53 +0100

    add: class

--- resources/views/blocks/numbers/styles/number-1.blade.php
+++ resources/views/blocks/numbers/styles/number-1.blade.php
@@ -1,40 +1,39 @@
 <div class="container s-number">
-  <div class="s-number__title">
-    <x-title :tag="$number1['title']['tag']">
-      {!! $number1['title']['value'] !!}
-    </x-title>
-  </div>
-
-  <div>
-    {!! $number1['content'] !!}
-  </div>
-
-  <div class="s-number-layout">
-  @foreach($number1['numbers'] as $key => $number)
-    <div class="s-number-layout-inner">
-    <x-image :lg="$number['icon']"/>
-      <div class="s-number-layout-inner__number">
-        {!! $number['number'] !!}
-      </div>
-
-      <div class="s-number-layout-inner__text">
-        {!! $number['description'] !!}
-      </div>
-
+    <div class="s-number__title">
+        <x-title :tag="$number1['title']['tag']">
+            {!! $number1['title']['value'] !!}
+        </x-title>
     </div>
-  @endforeach
-</div>
 
-  @if($number1['button'] && $number1['button']['link'])
-    <div class="s-number__button">
-        <x-akyos-blocks::aky-button
-            :appearance="$number1['button']['color']"
-            :icon="$number1['button']['icon']"
-            :iconposition="$number1['button']['iconposition']"
-            :href="$number1['button']['link']['url']"
-        >
-            {{ $number1['button']['link']['title'] }}
-        </x-akyos-blocks::aky-button>
+    <div class="s-number__description">
+        {!! $number1['content'] !!}
     </div>
-  @endif
+
+    <div class="s-number-layout">
+        @foreach($number1['numbers'] as $key => $number)
+            <div class="s-number-layout-inner">
+                <x-image :lg="$number['icon']"/>
+                <div class="s-number-layout-inner__number">
+                    {!! $number['number'] !!}
+                </div>
+
+                <div class="s-number-layout-inner__text">
+                    {!! $number['description'] !!}
+                </div>
+            </div>
+        @endforeach
+    </div>
+
+    @if($number1['button'] && $number1['button']['link'])
+        <div class="s-number__button">
+            <x-akyos-blocks::aky-button
+                :appearance="$number1['button']['color']"
+                :icon="$number1['button']['icon']"
+                :iconposition="$number1['button']['iconposition']"
+                :href="$number1['button']['link']['url']"
+            >
+                {{ $number1['button']['link']['title'] }}
+            </x-akyos-blocks::aky-button>
+        </div>
+    @endif
 </div>
