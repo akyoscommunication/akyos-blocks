5162fbe3caac88dc0037a96dfcc1c8451315aa43
Kévin Groux <kevingroux@mac-mini-de-kevin.home>
2024-11-18 09:53:14 +0100

    fix: content 12

--- resources/views/blocks/contents/styles/content-12.blade.php
+++ resources/views/blocks/contents/styles/content-12.blade.php
@@ -1,43 +1,45 @@
 <div class="container s-content">
-    <x-title :tag="$content12['title']['tag']">
-		 {!! $content12['title']['value'] !!}
-	 </x-title>
-	<ul class="s-content-tab-filters" data-filters="#ContentFiltered{{ $block['id'] }}">
-		@foreach($content12['tabs'] as $key => $tab)
-			<li class="c-filter" data-filter="tab-{{ $key }}">{!! $tab['title'] !!}</li>
-		@endforeach
-	</ul>
-	<div id="ContentFiltered{{ $block['id'] }}" class="s-content-layout">
-		@foreach($content12['tabs'] as $key => $tab)
-			<div class="s-content-layout__text" data-filtered="tab-{{ $key }}">
-				{!! $tab['content'] !!}
-			</div>
-			<div class="s-content-layout-column" data-filtered="tab-{{ $key }}">
-				@foreach($tab['content-tab'] as $key2 =>  $tab_inner)
-					<div class="s-content-layout-inner" data-filtered="tab-{{ $key }}">
-						<x-title :tag="$tab_inner['title']['tag']">
-							{!! $tab_inner['title']['value'] !!}
-						</x-title>
-						<div class="s-content-slider">
-							<x-akyos-blocks::aky-slider name="slider--content12{{$block['id'] . $key2 . $key}}"
-																 navigation="arrow"
-																 pagination="1" scrollbar="1" per="1" permd="1"
-																 persm="1"
-																 perxs="1"
-																 autoheight="true" gap="0"
-																 sliderid="{{$block['id'] . $key2 . $key}}"
-																 class="s-content-layout-inner__img">
-								@if($tab_inner['images'])
-									@foreach($tab_inner['images'] as $key3 => $image)
-										<div class="s-content__image swiper-slide" data-filtered="tab-{{ $key }}">
-											<x-image :lg="$image['image']"/>
-										</div>
-									@endforeach
-								@endif
-							</x-akyos-blocks::aky-slider>
-						</div>
-						<div class="s-content-layout-inner__text">
-							{!! $tab_inner['description'] !!}
+    @if($content12['title'] && $content12['title']['value'])
+        <x-title :tag="$content12['title']['tag']">
+            {!! $content12['title']['value'] !!}
+        </x-title>
+    @endif
+    <ul class="s-content-tab-filters" data-filters="#ContentFiltered{{ $block['id'] }}">
+        @foreach($content12['tabs'] as $key => $tab)
+            <li class="c-filter" data-filter="tab-{{ $key }}">{!! $tab['title'] !!}</li>
+        @endforeach
+    </ul>
+    <div id="ContentFiltered{{ $block['id'] }}" class="s-content-layout">
+        @foreach($content12['tabs'] as $key => $tab)
+            <div class="s-content-layout__text" data-filtered="tab-{{ $key }}">
+                {!! $tab['content'] !!}
+            </div>
+            <div class="s-content-layout-column" data-filtered="tab-{{ $key }}">
+                @foreach($tab['content-tab'] as $key2 =>  $tab_inner)
+                    <div class="s-content-layout-inner" data-filtered="tab-{{ $key }}">
+                        <x-title :tag="$tab_inner['title']['tag']">
+                            {!! $tab_inner['title']['value'] !!}
+                        </x-title>
+                        <div class="s-content-slider">
+                            <x-akyos-blocks::aky-slider name="slider--content12{{$block['id'] . $key2 . $key}}"
+                                                        navigation="arrow"
+                                                        pagination="1" scrollbar="1" per="1" permd="1"
+                                                        persm="1"
+                                                        perxs="1"
+                                                        autoheight="true" gap="0"
+                                                        sliderid="{{$block['id'] . $key2 . $key}}"
+                                                        class="s-content-layout-inner__img">
+                                @if($tab_inner['images'])
+                                    @foreach($tab_inner['images'] as $key3 => $image)
+                                        <div class="s-content__image swiper-slide" data-filtered="tab-{{ $key }}">
+                                            <x-image :lg="$image['image']"/>
+                                        </div>
+                                    @endforeach
+                                @endif
+                            </x-akyos-blocks::aky-slider>
+                        </div>
+                        <div class="s-content-layout-inner__text">
+                            {!! $tab_inner['description'] !!}
 
                             @if($tab_inner['button'] && $tab_inner['button']['link'])
                                 <x-akyos-blocks::aky-button :appearance="$tab_inner['button']['color']"
@@ -47,14 +49,14 @@
                                     {{ $tab_inner['button']['link']['title'] }}
                                 </x-akyos-blocks::aky-button>
                             @endif
-						</div>
-					</div>
-				@endforeach
-			</div>
-		@endforeach
-	</div>
-	<div class="s-content-description">
-		{!! $content12['description'] !!}
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        @endforeach
+    </div>
+    <div class="s-content-description">
+        {!! $content12['description'] !!}
     </div>
 </div>
 
