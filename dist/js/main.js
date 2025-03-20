5c47a5bec807e189dd060586fb817e48f8b9e642
guillaumeluce <guillaume@akyos.com>
2025-03-20 15:28:08 +0100

    remove: mix

---
+++ dist/js/main.js
@@ -0,0 +1,35 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./resources/assets/js/main.js":
+/*!*************************************!*\
+  !*** ./resources/assets/js/main.js ***!
+  \*************************************/
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ "./resources/assets/css/main.scss":
+/*!****************************************!*\
+  !*** ./resources/assets/css/main.scss ***!
+  \****************************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: 2 arguments required, but only 1 was passed.\n  ╷\n2 │   padding: rem(60) 0;\n  │            ^^^^^^^\n  ╵\n  resources/assets/css/blocks/_accordion1.scss 2:12  @import\n  blocks/*.scss 1:9                                  @import\n  resources/assets/css/main.scss 1:9                 root stylesheet\n    at processResult (/Users/guillaumeluce/Documents/AKYOS/akyos-blocks/node_modules/webpack/lib/NormalModule.js:890:19)\n    at /Users/guillaumeluce/Documents/AKYOS/akyos-blocks/node_modules/webpack/lib/NormalModule.js:1036:5\n    at /Users/guillaumeluce/Documents/AKYOS/akyos-blocks/node_modules/loader-runner/lib/LoaderRunner.js:400:11\n    at /Users/guillaumeluce/Documents/AKYOS/akyos-blocks/node_modules/loader-runner/lib/LoaderRunner.js:252:18\n    at context.callback (/Users/guillaumeluce/Documents/AKYOS/akyos-blocks/node_modules/loader-runner/lib/LoaderRunner.js:124:13)\n    at Object.loader (/Users/guillaumeluce/Documents/AKYOS/akyos-blocks/node_modules/sass-loader/dist/index.js:69:5)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	__webpack_modules__["./resources/assets/js/main.js"]();
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./resources/assets/css/main.scss"]();
+/******/ 	
+/******/ })()
+;
